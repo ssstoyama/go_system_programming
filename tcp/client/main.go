04831a766c493abf8ddab5a06612b54209759f96
toyama <shouya.toyama@gmail.com>
2023-01-28 10:53:32 +0000

    gzip

--- tcp/client/main.go
+++ tcp/client/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"os"
 	"strings"
 )
 
-// Keep-Alive 対応版
+// gzip 対応版
 func main() {
 	sendMessages := []string{
 		"ASCII",
@@ -33,6 +36,9 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		// gzip 対応クライアントであることをサーバーに伝える
+		request.Header.Set("Accept-Encoding", "gzip")
+
 		request.Write(conn)
 		response, err := http.ReadResponse(bufio.NewReader(conn), request)
 		if err != nil {
@@ -41,12 +47,29 @@ func main() {
 			conn = nil
 			continue
 		}
-		dump, err := httputil.DumpResponse(response, true)
+
+		// DumpResponse は圧縮された内容を理解できないため false で Body を無視する
+		dump, err := httputil.DumpResponse(response, false)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(strings.Repeat("-", 30))
 		fmt.Println(string(dump))
+
+		defer response.Body.Close()
+		if response.Header.Get("Content-Encoding") == "gzip" {
+			// gzip 対応サーバーからのレスポンス処理
+			reader, err := gzip.NewReader(response.Body)
+			if err != nil {
+				panic(err)
+			}
+			io.Copy(os.Stdout, reader)
+			reader.Close()
+		} else {
+			io.Copy(os.Stdout, response.Body)
+		}
+		fmt.Println()
+
 		current++
 		if current >= len(sendMessages) {
 			break
--- tcp/server/main.go
+++ tcp/server/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 	"time"
 )
 
-// Keep-Alive 対応版
+// gzip 対応版
 func main() {
 	listener, err := net.Listen("tcp", "localhost:8888")
 	if err != nil {
@@ -25,43 +27,61 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			// Accept 後のソケットで何度も応答を返すためにループ
-			// TCP のコネクションが張られたあと何度もリクエストを受けられる
-			for {
-				// 通信がしばらくない場合タイムアウトする
-				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		go processSession(conn)
+	}
+}
 
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-				if err != nil {
-					neterr, ok := err.(net.Error)
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-				dump, err := httputil.DumpRequest(request, true)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(dump))
+func isGZipAcceptable(request *http.Request) bool {
+	return strings.Index(
+		strings.Join(request.Header["Accept-Encoding"], ","),
+		"gzip",
+	) != -1
+}
 
-				// レスポンスを書き込む
-				// HTTP/1.1 かつ、ContentLength の設定が必要
-				response := http.Response{
-					StatusCode:    http.StatusOK,
-					ProtoMajor:    1,
-					ProtoMinor:    1,
-					ContentLength: int64(len(dump)),
-					Body:          ioutil.NopCloser(bytes.NewReader(dump)),
-				}
-				response.Write(conn)
+func processSession(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+	for {
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
 			}
-		}()
+			panic(err)
+		}
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		// レスポンスを書き込む
+		// HTTP/1.1 かつ、ContentLength の設定が必要
+		response := http.Response{
+			StatusCode: http.StatusOK,
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header:     make(http.Header),
+		}
+		if isGZipAcceptable(request) {
+			content := fmt.Sprintf("%s (gzipped)\n", dump)
+			var buf bytes.Buffer
+			writer := gzip.NewWriter(&buf)
+			io.WriteString(writer, content)
+			writer.Close()
+			response.Body = ioutil.NopCloser(&buf)
+			response.ContentLength = int64(buf.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		} else {
+			content := fmt.Sprintf("%s\n", dump)
+			response.Body = ioutil.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+		response.Write(conn)
 	}
 }
